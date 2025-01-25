35f847464eaf0a2fb42b26b00ed327e866a76a88
LeonFZ <145753729+LeonFZ@users.noreply.github.com>
2025-01-25 12:25:25 +0100

    floating moating

--- src/character/code/character.gd
+++ src/character/code/character.gd
@@ -3,19 +3,50 @@ extends CharacterBody2D
 var selected = false
 var mouse_offset = Vector2(0, 0)
 
-func _ready():
-	pass
+var float_speed: float = 2.0
+var float_amplitude: float = 10.0
 
-func _process(_delta):
+var original_position: Vector2
+var time: float = 0.0
+
+var scaleSize: float = 0.3
+var upScaledSize: float = 0.35
+
+var selectedColor : Color = Color(1, 0, 1)
+var hoveringColor : Color = Color(0, 1, 0)
+var standardColor : Color = Color(1, 1, 1)
+
+
+func _ready():
+	self.scale = Vector2(scaleSize, scaleSize)
+	original_position = position
+	z_index = 1
+	
+
+func _process(delta):
 	if selected:
+		z_index = 2
 		followMouse()
-	if Input.is_action_pressed("arrow_left"):
-		changeEmoji()
+		original_position = position
+		self.scale = Vector2(upScaledSize, upScaledSize)
+	else:
+		z_index = 1
+		time += delta * float_speed
+		var offset_y = float_amplitude * sin(time)
+		position.y = original_position.y + offset_y
+		self.scale = Vector2(scaleSize, scaleSize)
+
+func _physics_process(delta):
+	var moveVelocity = Vector2.ZERO
+	moveVelocity.y += 10 * delta
+	velocity = moveVelocity
+
+	move_and_slide()
+
 
 func _on_area_2d_input_event(_viewport:Node, event:InputEvent, _shape_idx:int):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			print("pressed")
 			mouse_offset = position - get_global_mouse_position()
 			selected = true
 		else:
@@ -24,6 +55,7 @@ func _on_area_2d_input_event(_viewport:Node, event:InputEvent, _shape_idx:int):
 				if (body.get_groups().has("character") and (body != self and !body.selected)):
 						print("collision")
 
+
 					
 func followMouse():
 	position = get_global_mouse_position() + mouse_offset
@@ -31,3 +63,12 @@ func followMouse():
 func changeEmoji():
 	$EmojiPlaceholder.texture = load("res://src/character/assets/emojis/emoji_attention.png")
 
+func _on_area_2d_area_entered(area):
+	area.get_parent().modulate = hoveringColor
+	$Sprite2D.modulate = hoveringColor
+
+
+func _on_area_2d_area_exited(area):
+	area.get_parent().modulate = standardColor
+	$Sprite2D.modulate = standardColor
+	
