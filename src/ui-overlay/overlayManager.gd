7c2cad503bfa2bf3084e5496e294e5102158c838
NicoheroesDX <nico@schulten-online.de>
2025-01-26 14:34:16 +0100

    Added logs

--- src/ui-overlay/overlayManager.gd
+++ src/ui-overlay/overlayManager.gd
@@ -6,14 +6,28 @@ class_name StatBars
 @onready var materialBar : TextureProgressBar = $CanvasGroup/materialBar/TextureProgressBar;
 @onready var foodBar : TextureProgressBar = $CanvasGroup/foodBar/TextureProgressBar;
 
+@onready var logBase : Control = %LogBase
+@onready var logLabel : RichTextLabel = %LogLabel
+
 @onready var pool = %PoolGrid;
 @onready var back = $Backdrop;
+@onready var scroll = $ScrollContainer;
 @onready var close = $CloseButton;
+
+func _ready() -> void:
+	GameState.showLog.connect(updateLabel)
+
+func updateLabel(logText: String):
+	$CanvasGroup/LogBase/Timer.wait_time = 3;
+	logBase.visible = true
+	logLabel.text = logText
+	$CanvasGroup/LogBase/Timer.start();
 
 func toggleInventory(isOpen: bool):
 	pool.visible = isOpen;
 	back.visible = isOpen;
 	close.visible = isOpen;
+	scroll.visible = isOpen;
 	$Button/Click.play()
 
 func _on_button_pressed() -> void:
@@ -30,3 +44,6 @@ func setStats(optimism: int, o2: int, food: int, material: int):
 	o2Bar.value = o2
 	foodBar.value = food
 	materialBar.value = material
+
+func _on_timer_timeout() -> void:
+	logBase.visible = false;
