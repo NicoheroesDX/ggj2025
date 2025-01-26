55d15f60882bc60a525bca79288f4205c05c72b6
Hetzel, Tatiana <tatiana.hetzel@bredex.de>
2025-01-26 01:18:18 +0100

    new thoughts, stat progress bar script

--- src/ui-overlay/overlay.gd
+++ src/ui-overlay/overlay.gd
@@ -1,6 +1,17 @@
 extends CanvasLayer
+@onready var optimismBar : TextureProgressBar = $CanvasGroup/foodBar/TextureProgressBar # : Bar
+@onready var o2Bar : TextureProgressBar = $CanvasGroup/o2Bar/TextureProgressBar;
+@onready var materialBar : TextureProgressBar = $CanvasGroup/materialBar/TextureProgressBar;
+@onready var foodBar : TextureProgressBar = $CanvasGroup/foodBar/TextureProgressBar;
 
-@onready var pool = %PoolGrid;
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
 
-func _on_button_pressed() -> void:
-	pool.visible = !pool.visible;
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func changeStats(optimism: int, o2: int, food: int, material: int):
+	optimismBar.FILL_CLOCKWISE
+	pass;
